91276cc0c447f8928664dc4fa0c7beec33ee5dd2
Tyrone Fontaine <tyrone@blackplastic.ca>
2012-02-29 22:16:46 -0500

    updated jquery to google 1.7.1 min

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 	<title>Blank XT</title>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
 <!--<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />-->
-	<script type="text/javascript" src="js/jquery-1.4.4.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
 	<link rel="stylesheet" type="text/css" href="css/reset.css" media="all" />
 	<link rel="stylesheet" type="text/css" href="css/default.css" media="all" />
