234e76f55623abc013f8ca2c8b7f9ae30ca1a471
Tyrone Fontaine <tyrone@blackplastic.ca>
2012-02-29 22:07:15 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru">
+<head>
+	<title>Blank XT</title>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+<!--<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />-->
+	<script type="text/javascript" src="js/jquery-1.4.4.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/reset.css" media="all" />
+	<link rel="stylesheet" type="text/css" href="css/default.css" media="all" />
+	<script type="text/javascript"></script>
+	<style type="text/css"></style>
+</head>
+<body>
+	<!-- insert content here -->
+</body>
+</html>
